41299806e421017796dcd95e4fd23152d97eec92
Walter Stanish <walter@stani.sh>
2016-09-27 03:34:18 +0800

    Better demo

--- main.lua
+++ main.lua
@@ -13,10 +13,10 @@ resolutionPixelsY = love.graphics.getHeight()
 
 function love.load()
 	pic = svglover_load('demo.svg')
-	svglover_display(pic,100,100,100,100,true)
-	svglover_display(pic,350,200,200,200,true)
-	svglover_display(pic,850,50,150,300,true)
-	svglover_display(pic,600,600,250,250,true)
+	svglover_display(pic,100,100,100,100,true,{255,0,0,255},1)
+	svglover_display(pic,350,200,200,200,true,{0,255,0,255},4)
+	svglover_display(pic,850,50,150,300,true,{255,255,0,128},10)
+	svglover_display(pic,600,600,250,250,true,{0,0,255,128},20)
 end
 
 function love.draw()
